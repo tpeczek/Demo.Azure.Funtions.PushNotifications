11ba9aaf6183144395f9f3aaf0a12d9ea277bba9
Tomasz Pęczek <tpeczek@gmail.com>
2019-02-01 21:55:28 +0100

    Utilizing Lib.Azure.WebJobs.Extensions.WebPush

--- Demo.Azure.Funtions.PushNotifications/SendNotificationFunction.cs
+++ Demo.Azure.Funtions.PushNotifications/SendNotificationFunction.cs
@@ -6,30 +6,33 @@ using Microsoft.Azure.Documents.Linq;
 using Microsoft.Azure.Documents.Client;
 using Lib.Net.Http.WebPush;
 using Lib.Net.Http.WebPush.Authentication;
+using Lib.Azure.WebJobs.Extensions.WebPush.Bindings;
 
 namespace Demo.Azure.Funtions.PushNotifications
 {
     public static class SendNotificationFunction
     {
-        private static readonly PushServiceClient _pushClient = new PushServiceClient
-        {
-            DefaultAuthentication = new VapidAuthentication("<Application Server Public Key>", "<Application Server Private Key>")
-            {
-                Subject = "https://localhost:65506/"
-            }
-        };
         private static readonly Uri _subscriptionsCollectionUri = UriFactory.CreateDocumentCollectionUri("PushNotifications", "SubscriptionsCollection");
 
         [FunctionName("SendNotification")]
-        public static async Task Run(
-            [CosmosDBTrigger("PushNotifications", "NotificationsCollection", LeaseCollectionName = "NotificationsLeaseCollection", CreateLeaseCollectionIfNotExists = true, ConnectionStringSetting = "CosmosDBConnection")]
-            IReadOnlyList<PushMessage> notifications,
-            [CosmosDB("PushNotifications", "SubscriptionsCollection", ConnectionStringSetting = "CosmosDBConnection")]
-            DocumentClient client)
+        public static async Task Run([CosmosDBTrigger(
+            databaseName: "PushNotifications",
+            collectionName: "NotificationsCollection",
+            ConnectionStringSetting = "CosmosDBConnection",
+            LeaseCollectionName = "NotificationsLeaseCollection",
+            CreateLeaseCollectionIfNotExists = true)]IReadOnlyList<PushMessage> notifications,
+            [CosmosDB(
+            databaseName: "PushNotifications",
+            collectionName: "SubscriptionsCollection",
+            ConnectionStringSetting = "CosmosDBConnection")]DocumentClient cosmosDbClient,
+            [PushService(
+            PublicKeySetting = "ApplicationServerPublicKey",
+            PrivateKeySetting = "ApplicationServerPrivateKey",
+            SubjectSetting = "ApplicationServerSubject")]PushServiceClient pushServiceClient)
         {
             if (notifications != null)
             {
-                IDocumentQuery<PushSubscription> subscriptionQuery = client.CreateDocumentQuery<PushSubscription>(_subscriptionsCollectionUri, new FeedOptions
+                IDocumentQuery<PushSubscription> subscriptionQuery = cosmosDbClient.CreateDocumentQuery<PushSubscription>(_subscriptionsCollectionUri, new FeedOptions
                 {
                     EnableCrossPartitionQuery = true,
                     MaxItemCount = -1
@@ -42,7 +45,7 @@ namespace Demo.Azure.Funtions.PushNotifications
                         foreach (PushMessage notification in notifications)
                         {
                             // Fire-and-forget
-                            _pushClient.RequestPushMessageDeliveryAsync(subscription, notification);
+                            pushServiceClient.RequestPushMessageDeliveryAsync(subscription, notification);
                         }
                     }
                 }
