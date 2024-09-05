4fb50966ac64973c888a98e06f0dd8f350aed1d4
Tomasz Pęczek <tpeczek@gmail.com>
2024-09-05 21:26:25 +0200

    Implementing an isolated worker model demo

--- Demo.Azure.Functions.Worker.PushNotifications/SendNotificationFunction.cs
+++ Demo.Azure.Functions.Worker.PushNotifications/SendNotificationFunction.cs
@@ -1,6 +1,8 @@
 using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using Lib.Net.Http.WebPush;
+using Lib.Azure.Functions.Worker.Extensions.WebPush;
 
 namespace Demo.Azure.Functions.Worker.PushNotifications
 {
@@ -14,7 +16,7 @@ namespace Demo.Azure.Functions.Worker.PushNotifications
         }
 
         [Function("SendNotificationFunction")]
-        public void Run([CosmosDBTrigger(
+        public async Task Run([CosmosDBTrigger(
             databaseName: "PushNotifications",
             containerName: "Notifications",
             Connection = "CosmosDBConnection",
@@ -23,11 +25,34 @@ namespace Demo.Azure.Functions.Worker.PushNotifications
             [CosmosDBInput(
             databaseName: "PushNotifications",
             containerName: "Subscriptions",
-            Connection = "CosmosDBConnection")] CosmosClient cosmosClient)
+            Connection = "CosmosDBConnection")] CosmosClient cosmosClient,
+            [PushServiceInput(
+            PublicKeySetting = "ApplicationServerPublicKey",
+            PrivateKeySetting = "ApplicationServerPrivateKey",
+            SubjectSetting = "ApplicationServerSubject")] PushServiceClient pushServiceClient)
         {
             if (notifications != null)
             {
                 Container subscriptionsContainer = cosmosClient.GetDatabase("PushNotifications").GetContainer("Subscriptions");
+                using (FeedIterator<PushSubscription> subscriptionsIterator = subscriptionsContainer.GetItemQueryIterator<PushSubscription>())
+                {
+                    while (subscriptionsIterator.HasMoreResults)
+                    {
+                        foreach (PushSubscription subscription in await subscriptionsIterator.ReadNextAsync())
+                        {
+                            foreach (Notification notification in notifications)
+                            {
+                                // Fire-and-forget
+                                pushServiceClient.RequestPushMessageDeliveryAsync(subscription, new PushMessage(notification.Content)
+                                {
+                                    Topic = notification.Topic,
+                                    TimeToLive = notification.TimeToLive,
+                                    Urgency = notification.Urgency
+                                });
+                            }
+                        }
+                    }
+                }
             }
         }
     }
@@ -40,6 +65,6 @@ namespace Demo.Azure.Functions.Worker.PushNotifications
 
         public int? TimeToLive { get; set; }
 
-        //public PushMessageUrgency Urgency { get; set; }
+        public PushMessageUrgency Urgency { get; set; }
     }
 }
